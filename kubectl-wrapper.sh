474c37a9faf4c806d7e01e19717b5faf07275b35
djeeno <29125616+djeeno@users.noreply.github.com>
2020-03-25 18:48:19 +0900

    Update kubectl-wrapper.sh

--- kubectl-wrapper.sh
+++ kubectl-wrapper.sh
@@ -1,7 +1,7 @@
 # Run the `source` command to load this file into the current shell like follow:
 #   $ source kubectl-wrapper.sh
 
-# kubect-wrapper displays the current-context and prompts for user confirmation 
+# kubect-wrapper displays the current-context and prompts for user confirmation
 # when trying to execute kubectl's subcommands that can update the cluster's state.
 kubectl() { (
   # if has 1 argument || has -h --help --dry-run option || Sub-commands that are not dangerous if executed without confirmation; then
